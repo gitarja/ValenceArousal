7045cec032b997dd1856c0bf1ddc5137f270116b
Prasetia Utama Putra <prasetiautama2@gmail.com>
2020-08-20 16:15:04 +0900

    Update Features

--- StatisticalAnalysis/FeaturesAnalysis.py
+++ StatisticalAnalysis/FeaturesAnalysis.py
@@ -5,34 +5,44 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import mutual_info_classif
 
-path = "D:\\usr\\pras\\data\\EmotionTestVR\\Okada\\"
-eeg_path = path +"results\\EEG\\"
-gsr_path = path +"results\\GSR\\"
-resp_path = path +"results\\Resp\\"
 
-features_list = pd.read_csv(path+"features_list.csv")
-
+subjects = {"Okada", "Nishiwaki", "Komiya"}
 features = []
-features_list["Valence"] = features_list["Valence"].apply(valArLevelToLabels)
-features_list["Arousal"] = features_list["Arousal"].apply(valArLevelToLabels)
-for i in range(len(features_list)):
-    filename = features_list.iloc[i]["Idx"]
-    eda_features = np.load(gsr_path+"eda_"+str(filename)+".npy")
-    ppg_features = np.load(gsr_path + "ppg_" + str(filename) + ".npy")
-    resp_features = np.load(resp_path + "resp_" + str(filename) + ".npy")
-    eeg_features = np.load(eeg_path + "eeg_" + str(filename) + ".npy")
+y_ar = []
+y_val = []
 
-    features.append(np.concatenate([eda_features, ppg_features, resp_features, eeg_features]))
+for s in subjects:
+    path = "D:\\usr\\pras\\data\\EmotionTestVR\\"+s+"\\"
+    eeg_path = path +"results\\EEG\\"
+    gsr_path = path +"results\\GSR\\"
+    resp_path = path +"results\\Resp\\"
+
+    features_list = pd.read_csv(path+"features_list.csv")
+    features_list["Valence"] = features_list["Valence"].apply(valArLevelToLabels)
+    features_list["Arousal"] = features_list["Arousal"].apply(valArLevelToLabels)
+    for i in range(len(features_list)):
+        filename = features_list.iloc[i]["Idx"]
+        eda_features = np.load(gsr_path+"eda_"+str(filename)+".npy")
+        ppg_features = np.load(gsr_path + "ppg_" + str(filename) + ".npy")
+        resp_features = np.load(resp_path + "resp_" + str(filename) + ".npy")
+        eeg_features = np.load(eeg_path + "eeg_" + str(filename) + ".npy")
+
+        features.append(np.concatenate([eda_features, ppg_features, resp_features, eeg_features]))
+
+    y_ar.append(features_list["Arousal"].values)
+    y_val.append(features_list["Valence"].values)
+
 
 #concatenate features and normalize them
 X = np.concatenate([features])
 scaler = StandardScaler()
 X_norm = scaler.fit_transform(X)
 
+y_ar = np.concatenate(y_ar)
+y_val = np.concatenate(y_val)
 
-y_ar = features_list["Arousal"].values
-y_val = features_list["Valence"].values
 
 
 #features length
@@ -43,37 +53,57 @@ eeg_length = eeg_features.shape[0]
 
 #Analyze arousal
 # Build a forest and compute the impurity-based feature importances
-forest_ar = ExtraTreesClassifier(n_estimators=250,
-                              random_state=0)
+X_ar_train, X_ar_test, y_ar_train, y_ar_test = train_test_split(X_norm, y_ar, test_size=0.3, random_state=42)
+forest_ar = ExtraTreesClassifier(n_estimators=50,
+                              random_state=0, max_depth=7, class_weight={0: 0.75, 1: 1.15})
 
-X_ar_train, X_ar_test, y_ar_train, y_ar_test = train_test_split(X_norm, y_ar, test_size=0.5, random_state=42)
 forest_ar.fit(X_ar_train, y_ar_train)
 print(forest_ar.score(X_ar_test, y_ar_test))
 
-features_impt_ar = np.array([np.sum(forest_ar.feature_importances_[0:eda_length]),
-                          np.sum(forest_ar.feature_importances_[eda_length:eda_length+ppg_length]),
-                          np.sum(forest_ar.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
-                          np.sum(forest_ar.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
+features_impt_ar = np.array([np.average(forest_ar.feature_importances_[0:eda_length]),
+                          np.average(forest_ar.feature_importances_[eda_length:eda_length+ppg_length]),
+                          np.average(forest_ar.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
+                          np.average(forest_ar.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
 print(features_impt_ar)
 plt.bar(np.arange(len(features_impt_ar)), features_impt_ar)
-
 plt.show()
 
 
 #Analyze valence
-forest_val = ExtraTreesClassifier(n_estimators=250,
-                              random_state=0)
+X_val_train, X_val_test, y_val_train, y_val_test = train_test_split(X_norm, y_val, test_size=0.3, random_state=42)
+forest_val = ExtraTreesClassifier(n_estimators=50,
+                              random_state=0, max_depth=7, class_weight={0: 0.75, 1: 1.15})
 
-X_val_train, X_val_test, y_val_train, y_val_test = train_test_split(X_norm, y_val, test_size=0.5, random_state=42)
 forest_val.fit(X_val_train, y_val_train)
 print(forest_val.score(X_val_test, y_val_test))
 
-features_impt_val = np.array([np.sum(forest_val.feature_importances_[0:eda_length]),
-                          np.sum(forest_val.feature_importances_[eda_length:eda_length+ppg_length]),
-                          np.sum(forest_val.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
-                          np.sum(forest_val.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
+features_impt_val = np.array([np.average(forest_val.feature_importances_[0:eda_length]),
+                          np.average(forest_val.feature_importances_[eda_length:eda_length+ppg_length]),
+                          np.average(forest_val.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
+                          np.average(forest_val.feature_importances_[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
 print(features_impt_val)
+
+
 plt.bar(np.arange(len(features_impt_val)), features_impt_val)
+plt.show()
+
+#Mutual informatiorn
+mi_ar = mutual_info_classif(X_norm, y_ar)
+mi_val = mutual_info_classif(X_norm, y_val)
+
+avg_mi_ar = np.array([np.average(mi_ar[0:eda_length]),
+                          np.average(mi_ar[eda_length:eda_length+ppg_length]),
+                          np.average(mi_ar[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
+                          np.average(mi_ar[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
+
+avg_mi_val = np.array([np.average(mi_val[0:eda_length]),
+                          np.average(mi_val[eda_length:eda_length+ppg_length]),
+                          np.average(mi_val[eda_length+ppg_length:eda_length+ppg_length+resp_length]),
+                          np.average(mi_val[eda_length+ppg_length:eda_length+ppg_length+resp_length+eeg_length])])
 
 
+plt.bar(np.arange(len(avg_mi_ar)), avg_mi_ar)
+plt.show()
+
+plt.bar(np.arange(len(avg_mi_val)), avg_mi_val)
 plt.show()
--- StatisticalAnalysis/FoldsAnalysis.py
+++ StatisticalAnalysis/FoldsAnalysis.py
@@ -33,7 +33,7 @@ for s in subjects:
         resp_features = np.load(resp_path_train + "resp_" + str(filename) + ".npy")
         eeg_features = np.load(eeg_path_train+ "eeg_" + str(filename) + ".npy")
 
-        features_train.append(np.concatenate([eeg_features]))
+        features_train.append(np.concatenate([eda_features, ppg_features, resp_features, eeg_features]))
 
     # label train
     y_ar_train.append(features_list_train["Arousal"].values)
@@ -65,7 +65,7 @@ for i in range(len(features_list_test)):
     resp_features = np.load(resp_path_test  + "resp_" + str(filename) + ".npy")
     eeg_features = np.load(eeg_path_test + "eeg_" + str(filename) + ".npy")
 
-    features_test.append(np.concatenate([eeg_features]))
+    features_test.append(np.concatenate([eda_features, ppg_features, resp_features, eeg_features]))
 
 #concatenate features and normalize them
 scaler = StandardScaler()
@@ -89,24 +89,26 @@ y_ar = np.concatenate([y_ar_train, y_ar_test])
 y_val = np.concatenate([y_val_train, y_val_test])
 
 #hyperparameters
-parameters = {"n_estimators": [50, 150, 200, 250], "max_depth": [3, 5, 7], 'class_weight':[{0:0.75, 1:1.15}, {0:0.75,1:1.5}]}
+parameters = {"n_estimators": [50, 150, 200, 250], "max_depth": [3, 5, 7], 'class_weight':[{0:0.75, 1:1.}, {0:1. ,1:1.}]}
 
 #Analyze arousal
 # Build a forest and compute the impurity-based feature importances
 
-X_ar_train, X_ar_test, y_ar_train, y_ar_test = train_test_split(X, y_ar, test_size=0.4, random_state=42)
+X_ar_train, X_ar_test, y_ar_train, y_ar_test = train_test_split(X, y_ar, test_size=0.3, random_state=42)
 
 forest_ar = ExtraTreesClassifier( random_state=0)
 clf_ar = GridSearchCV(forest_ar, parameters)
 clf_ar.fit(X_ar_train, y_ar_train)
+print(clf_ar.best_params_)
 print(clf_ar.score(X_ar_test, y_ar_test))
 print(confusion_matrix(y_ar_test, clf_ar.predict(X_ar_test), normalize='all') * 100)
 
 
 #Analyze valence
-X_val_train, X_val_test, y_val_train, y_val_test = train_test_split(X, y_val, test_size=0.4, random_state=42)
+X_val_train, X_val_test, y_val_train, y_val_test = train_test_split(X, y_val, test_size=0.3, random_state=42)
 forest_val = ExtraTreesClassifier(random_state=0)
 clf_val = GridSearchCV(forest_val, parameters)
 clf_val.fit(X_val_train, y_val_train)
+print(clf_ar.best_params_)
 print(clf_val.score(X_val_test, y_val_test))
 print(confusion_matrix(y_val_test, clf_val.predict(X_val_test), normalize='all') * 100)
