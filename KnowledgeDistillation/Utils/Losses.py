7745d9825a5bae0cc082bae8550334c97c314abe
Pras <prasetiautama2@gmail.com>
2021-02-21 17:06:13 +0900

    Update metric and losses regression

--- KnowledgeDistillation/Utils/Losses.py
+++ KnowledgeDistillation/Utils/Losses.py
@@ -11,19 +11,17 @@ class PCCLoss(tf.keras.losses.Loss):
         y_pred = ops.convert_to_tensor_v2(y_pred)
         y_true = math_ops.cast(y_true, y_pred.dtype)
 
-        y_pred_mean = math_ops.reduce_mean(y_pred, axis=-1)
-        y_true_mean = math_ops.reduce_mean(y_true, axis=-1,)
+        y_pred_mean = math_ops.reduce_mean(y_pred, keepdims=True)
+        y_true_mean = math_ops.reduce_mean(y_true, keepdims=True)
         y_pred_m = y_pred - y_pred_mean
         y_true_m = y_true - y_true_mean
-        y_pred_norm = tf.norm(y_pred_m, axis=-1)
-        y_true_norm = tf.norm(y_true_m, axis=-1)
+        y_pred_norm = tf.norm(y_pred_m, keepdims=True)
+        y_true_norm = tf.norm(y_true_m, keepdims=True)
 
-        # print(y_pred_m)
-        # print(y_true_m)
-        pcc = (math_ops.reduce_sum(y_pred_m * y_true_m, axis=-1)) / (
-                    math_ops.reduce_sum(y_pred_norm * y_true_norm, axis=-1) + 1e-25)
+        pcc = (math_ops.reduce_sum(y_pred_m * y_true_m, keepdims=True)) / (
+                math_ops.reduce_sum(y_pred_norm * y_true_norm, keepdims=True) + 1e-25)
 
-        # pcc = tf.minimum(tf.maximum(pcc, 1.0), -1.0)
+        pcc = tf.maximum(tf.minimum(pcc, 1.0), -1.0)
 
         return pcc
 
@@ -36,12 +34,13 @@ class CCCLoss(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
         y_pred = ops.convert_to_tensor_v2(y_pred)
         y_true = math_ops.cast(y_true, y_pred.dtype)
+
         y_pred = math_ops.round(y_pred)
         y_true = math_ops.round(y_true)
-        y_pred_mean = math_ops.reduce_mean(y_pred, axis=-1)
-        y_true_mean = math_ops.reduce_mean(y_true, axis=-1)
-        pred_std = math_ops.reduce_std(y_pred, axis=-1)
-        true_std = math_ops.reduce_std(y_true, axis=-1)
+        y_pred_mean = math_ops.reduce_mean(y_pred, keepdims=True)
+        y_true_mean = math_ops.reduce_mean(y_true, keepdims=True)
+        pred_std = math_ops.reduce_std(y_pred, keepdims=True)
+        true_std = math_ops.reduce_std(y_true, keepdims=True)
 
         pearson = self.pcc(y_true, y_pred)
         ccc_n = (2.0 * pearson * pred_std * true_std)
@@ -49,7 +48,7 @@ class CCCLoss(tf.keras.losses.Loss):
         ccc_d = (math_ops.square(pred_std) + math_ops.square(true_std) + math_ops.square(
             y_pred_mean - y_true_mean))
 
-        ccc = (ccc_n) / (ccc_d + 1e-25)
+        ccc = ccc_n / (ccc_d + 1e-25)
         return ccc
 
 
@@ -64,5 +63,6 @@ class SAGRLoss(tf.keras.losses.Loss):
         """
         y_pred = ops.convert_to_tensor_v2(y_pred)
         y_true = math_ops.cast(y_true, y_pred.dtype)
-        return K.mean(math_ops.sign(y_pred) == math_ops.sign(y_true), -1)
-
+        sagr = math_ops.cast(tf.math.equal(math_ops.sign(y_pred), math_ops.sign(y_true)), y_pred.dtype)
+        sagr = math_ops.reduce_mean(sagr)
+        return sagr
--- KnowledgeDistillation/Utils/Metrics.py
+++ KnowledgeDistillation/Utils/Metrics.py
@@ -4,6 +4,7 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.framework import ops
 import tensorflow as tf
+import numpy as np
 
 class PCC(Metric):
 
@@ -16,21 +17,21 @@ class PCC(Metric):
         y_pred = ops.convert_to_tensor_v2(y_pred)
         y_true = math_ops.cast(y_true, y_pred.dtype)
 
-        y_pred_mean = math_ops.reduce_mean(y_pred, axis=-1)
-        y_true_mean = math_ops.reduce_mean(y_true, axis=-1)
+        y_pred_mean = math_ops.reduce_mean(y_pred, keepdims=True)
+        y_true_mean = math_ops.reduce_mean(y_true,  keepdims=True)
         y_pred_m = y_pred - y_pred_mean
         y_true_m = y_true - y_true_mean
-        y_pred_norm = tf.norm(y_pred_m, axis=-1)
-        y_true_norm = tf.norm(y_true_m, axis=-1)
+        y_pred_norm = tf.norm(y_pred_m, keepdims=True)
+        y_true_norm = tf.norm(y_true_m, keepdims=True)
 
 
-        pcc = (math_ops.reduce_sum(y_pred_m * y_true_m, axis=-1)) / (
-                    math_ops.reduce_sum(y_pred_norm * y_true_norm, axis=-1) + 1e-25)
+        pcc = (math_ops.reduce_sum(y_pred_m * y_true_m, keepdims=True)) / (
+                    math_ops.reduce_sum(y_pred_norm * y_true_norm, keepdims=True) + 1e-25)
 
         pcc = tf.maximum(tf.minimum(pcc, 1.0), -1.0)
 
         self.pcc_r.assign_add(tf.reduce_sum(pcc))
-        self.total_count.assign_add(len(y_pred))
+        self.total_count.assign_add(len(pcc))
 
     def result(self):
         return math_ops.div_no_nan(self.pcc_r, self.total_count)
@@ -55,10 +56,10 @@ class CCC(Metric):
 
         y_pred = math_ops.round(y_pred)
         y_true = math_ops.round(y_true)
-        y_pred_mean = math_ops.reduce_mean(y_pred, axis=-1)
-        y_true_mean = math_ops.reduce_mean(y_true, axis=-1)
-        pred_std = math_ops.reduce_std(y_pred, axis=-1)
-        true_std = math_ops.reduce_std(y_true, axis=-1)
+        y_pred_mean = math_ops.reduce_mean(y_pred, keepdims=True)
+        y_true_mean = math_ops.reduce_mean(y_true, keepdims=True)
+        pred_std = math_ops.reduce_std(y_pred, keepdims=True)
+        true_std = math_ops.reduce_std(y_true, keepdims=True)
 
         pearson = self.pcc(y_true, y_pred)
         ccc_n = (2.0 * pearson * pred_std * true_std)
@@ -66,9 +67,9 @@ class CCC(Metric):
         ccc_d = (math_ops.square(pred_std) + math_ops.square(true_std) + math_ops.square(
                 y_pred_mean - y_true_mean))
 
-        ccc = (ccc_n) / (ccc_d+ 1e-25)
+        ccc = ccc_n / (ccc_d+ 1e-25)
         self.ccc_r.assign_add(tf.reduce_sum(ccc))
-        self.total_count.assign_add(len(y_pred))
+        self.total_count.assign_add(len(ccc))
 
 
     def result(self):
@@ -89,9 +90,9 @@ class SAGR(Metric):
         y_pred = ops.convert_to_tensor_v2(y_pred)
         y_true = math_ops.cast(y_true, y_pred.dtype)
         sagr = math_ops.cast(tf.math.equal(math_ops.sign(y_pred), math_ops.sign(y_true)), y_pred.dtype)
-        sagr = math_ops.reduce_mean(sagr, -1)
+        sagr = math_ops.reduce_mean(sagr, keepdims=True)
         self.sagr_r.assign_add(tf.reduce_sum(sagr))
-        self.total_count.assign_add(len(y_pred))
+        self.total_count.assign_add(len(sagr))
 
     def result(self):
         return math_ops.div_no_nan(self.sagr_r, self.total_count)
